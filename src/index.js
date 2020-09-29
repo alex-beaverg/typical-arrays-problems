451e4336b451df46bde7caa56cc2a75da01e3783
alex-beaverg <a.beaverg@gmail.com>
2020-09-30 00:23:20 +0300

    docs: upload task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  let x = 0;
+  let num;
+  if (array === undefined) return 0;
+  for (num of array) {
+    if (num < x) x = num;
+  }
+  return x;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let x = 0;
+  let num;
+  if (array === undefined) return 0;
+  for (num of array) {
+    if (num > x) x = num;
+  }
+  return x;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+  let x = 0;
+  let num;
+  if (array === undefined || array.length === 0) return 0;
+  for (num of array) {
+    x += num;
+  }  
+  return x / array.length;  
 }
